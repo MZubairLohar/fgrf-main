0db2beeb2aea91bd1b6159393d0a3d7ff4748b2b
Zubair <zbrlohar@gmail.com>
2025-02-21 20:28:31 +0500

    26

---
+++ frontend/components/newUi/complaintsProcedure/complaintsProcedureDoc.tsx
@@ -0,0 +1,121 @@
+const ComplaintsProcedureDocs = () => {
+  return (
+    <div className="bg-gray-200 py-5">
+      <div className="max-w-7xl mx-auto h-full px-10">
+        <h2 className="text-center font-bold text-teal-500 text-xl md:text-2xl lg:text-3xl py-3">
+          Complaints Procedure
+        </h2>
+        <ul className="relative">
+          <p className="text-start text-sm md:text-base py-3">
+            we are committed to providing the highest standard of service to all
+            our users. If you have a complaint about any aspect of our
+            organization or our services, we encourage you to bring it to our
+            attention. This Complaints Procedure outlines how complaints are
+            handled.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Submitting a Complaint
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            You can submit a complaint to us by emailing complaints@fgrf.uk or
+            by writing to us at Faizan Global Relief Foundation UK 284 WITTON
+            ROAD, BIRMINGHAM B6 6NX, United Kingdom. Please provide as much
+            detail as possible about your complaint, including any relevant
+            documents or evidence.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Acknowledgment of Complaint
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            Upon receiving your complaint, we will acknowledge receipt promptly,
+            usually within 07 working days. The acknowledgment will include
+            details of who will be handling your complaint and when you can
+            expect a response
+            {/* <ul className="pl-4 list-disc relative">
+              <li>To communicate with you and respond to your inquiries.</li>
+              <li className="text-sm md:text-base  text-start pt-2">
+                To provide you with information about our programs, events, and
+                activities.
+              </li>
+              <li className=" text-sm md:text-base  text-start py-2">
+                To process donations and other transactions.
+              </li>
+              <li className=" text-sm md:text-base  text-start pb-2">
+                To improve and personalize your experience on our website.
+              </li>
+            </ul> */}
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Investigation
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We will conduct a thorough investigation into your complaint. This
+            may involve gathering additional information, consulting relevant
+            parties, and reviewing our procedures or records.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Response
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We aim to provide a written response to your complaint within 07
+            working days of receiving it. Our response will address the issues
+            raised in your complaint and provide explanations, apologies where
+            necessary, and details of any actions we plan to take to resolve the
+            matter.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Escalation
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            f you are not satisfied with the initial response to your complaint,
+            you may request for it to be escalated to a higher level of
+            management within our organization. We will provide details of how
+            to escalate your complaint in our response.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Independent Review
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            If you remain dissatisfied after exhausting our internal complaints
+            procedure, you may have the right to seek an independent review from
+            an external body, such as a regulatory authority or ombudsman. We
+            will provide information about relevant external avenues for
+            complaint resolution if applicable.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Confidentiality
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We will treat all complaints and any personal information provided
+            in the course of the complaints procedure confidentially and in
+            accordance with our Privacy Policy.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Recording and Monitoring
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We will record details of all complaints received and monitor the
+            effectiveness of our complaints handling procedures to identify
+            areas for improvement.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Contact Us
+          </li>
+          <p className=" text-sm md:text-base  text-start py-2">
+            If you have a complaint or require further information about our
+            Complaints Procedure, please contact us at complaints@fgrf.uk or
+            write to us at Faizan Global Relief Foundation UK 284 WITTON ROAD,
+            BIRMINGHAM B6 6NX, United Kingdom.
+          </p>
+          <p className=" text-sm md:text-base  text-start py-2">
+            We are committed to resolving complaints promptly, fairly, and
+            transparently. Your feedback helps us improve our services and
+            ensure we meet the needs of our users.
+          </p>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ComplaintsProcedureDocs;
