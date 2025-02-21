aa056c1714d60075defe4e448dc519bccc9c376b
Zubair <zbrlohar@gmail.com>
2025-02-21 20:16:51 +0500

    16

---
+++ frontend/components/newUi/privacyPolicy/privacyDoc.tsx
@@ -0,0 +1,137 @@
+const PrivacyPolicyDocs = () => {
+  return (
+    <div className="bg-gray-200 py-5">
+      <div className="max-w-7xl mx-auto h-full px-10">
+        <h2 className="text-start font-bold text-teal-500 text-xl md:text-2xl lg:text-3xl py-3">
+          Privacy Policy
+        </h2>
+        <ul className="list-decimal relative ">
+          <p className="text-start text-sm md:text-base py-3">
+            This Privacy Policy outlines how FGRF (&#34;we&#34;, &#34;us&#34;,
+            or &#34;our&#34;), a nonprofit organization registered in the UK,
+            collects, uses, and protects your personal information in accordance
+            with the laws of the United Kingdom and Wales. By accessing or using
+            our website www.fgrf.uk, you consent to the practices described in
+            this Privacy Policy.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Information We Collect
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We may collect personal information from you when you voluntarily
+            provide it to us, such as when you fill out forms on our website,
+            subscribe to our newsletter, or make a donation. The types of
+            personal information we may collect include your name, email
+            address, mailing address, phone number, and payment information.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Use of Information
+          </li>
+          <div className=" text-sm md:text-base  text-start py-3">
+            We use the personal information we collect for the following
+            purposes:
+            <ul className="pl-4 list-disc relative">
+              <li>To communicate with you and respond to your inquiries.</li>
+              <li className="text-sm md:text-base  text-start pt-2">
+                To provide you with information about our programs, events, and
+                activities.
+              </li>
+              <li className=" text-sm md:text-base  text-start py-2">
+                To process donations and other transactions.
+              </li>
+              <li className=" text-sm md:text-base  text-start pb-2">
+                To improve and personalize your experience on our website.
+              </li>
+            </ul>
+          </div>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Legal Basis for Processing
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We will only process your personal information when we have a legal
+            basis to do so, such as when you have provided consent, when
+            processing is necessary for the performance of a contract, or when
+            processing is necessary for compliance with a legal obligation.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Disclosure of Information
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We do not sell, trade, or rent your personal information to third
+            parties. However, we may share your information with trusted service
+            providers who assist us in operating our website or conducting our
+            business, subject to appropriate confidentiality and security
+            measures
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Data Retention
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We will retain your personal information only for as long as
+            necessary to fulfill the purposes for which it was collected, or as
+            required by law. When we no longer need your personal information,
+            we will securely delete or anonymize it.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Your Rights
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            You have certain rights regarding your personal information under UK
+            and Wales law, including the right to access, correct, or delete
+            your information, the right to object to processing, and the right
+            to data portability. If you would like to exercise any of these
+            rights, please contact us using the contact information provided
+            below
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Cookies
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            Our website may use cookies and similar tracking technologies to
+            enhance your experience and analyze usage. You can control cookies
+            through your browser settings, but please note that disabling
+            cookies may affect your ability to use certain features of the
+            Website.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Links to Third-Party Websites
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            Our website may contain links to third-party websites. We are not
+            responsible for the privacy practices or content of these websites.
+            We encourage you to review the privacy policies of these sites
+            before providing any personal information.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Changes to Privacy Policy
+          </li>
+          <p className=" text-sm md:text-base  text-start py-3">
+            We reserve the right to update or change this Privacy Policy at any
+            time. Any changes will be To communicate with you and respond to
+            your inquiries. To provide you with information about our programs,
+            events, and activities. To process donations and other transactions.
+            To improve and personalize your experience on our website. effective
+            immediately upon posting the revised Privacy Policy on the Website.
+            We encourage you to review this Privacy Policy periodically for
+            updates.
+          </p>
+          <li className="font-semibold text-gray-600 text-sm md:text-base  text-start pt-3">
+            Contact Us
+          </li>
+          <p className=" text-sm md:text-base  text-start py-2">
+            If you have any questions or concerns about our Privacy Policy or
+            our practices regarding your personal information, please contact us
+            at info@fgrf.uk.
+          </p>
+          <p className=" text-sm md:text-base  text-start py-2">
+            By using our website, you agree to the terms of this Privacy Policy.
+            If you do not agree with the terms of this Privacy Policy, please do
+            not use our website.
+          </p>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicyDocs;
