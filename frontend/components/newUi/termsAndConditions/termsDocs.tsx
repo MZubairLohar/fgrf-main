2ea1cf8ec8f7c069203fa2dfd1bdd871b36a62e1
Zubair <zbrlohar@gmail.com>
2025-02-21 20:16:31 +0500

    15

---
+++ frontend/components/newUi/termsAndConditions/termsDocs.tsx
@@ -0,0 +1,71 @@
+const TermsAndConditionDocs = () => {
+  return (
+    <div className="bg-gray-200 py-5">
+      <div className="max-w-7xl mx-auto h-full px-10">
+        <h2 className="text-start font-bold text-teal-500 text-xl md:text-2xl lg:text-3xl py-3">
+          Use of this site is provided by FGRF, subject to the following Terms
+          and Conditions.
+        </h2>
+        <ul className="list-decimal relative ">
+          <li className="text-start text-sm md:text-base py-3">
+            Use of this site is provided by FGRF. By accessing the site, you
+            agree to the following Terms and Conditions.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            Using the site means you agree to these Terms and Conditions from
+            your first visit.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            FGRF can update these Terms and Conditions anytime. If you keep
+            using the site after changes, you&#39;re agreeing to the new terms.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            you agree to use this site only for lawful purposes and in a manner
+            which does not infringe the rights, or restrict, or inhibit the use
+            and enjoyment of the site by any third party.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            FGRF reserves the right to modify or update these copyright
+            restrictions at any time without prior notice.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            These copyright restrictions shall be governed by and construed in
+            accordance with the laws of the United Kingdom, without regard to
+            its conflict of law provisions.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            All content on the FGRF website, including but not limited to text,
+            graphics, logos, images, audio clips, and video clips, is the
+            property of FGRF or its licensors and is protected by copyright
+            laws.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            You may not reproduce, distribute, modify, transmit, perform, or
+            display any content from the FGRF website without the prior written
+            consent of FGRF, except as permitted by law
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            Limited License: FGRF grants you a limited, revocable,
+            non-exclusive, non-transferable license to access and use the
+            content on the FGRF website for personal, non-commercial use only,
+            subject to these Terms and any other applicable terms and
+            conditions.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            Limited License: FGRF grants you a limited, revocable,
+            non-exclusive, non-transferable license to access and use the
+            content on the FGRF website for personal, non-commercial use only,
+            subject to these Terms and any other applicable terms and
+            conditions.
+          </li>
+          <li className=" text-sm md:text-base  text-start py-3">
+            If you do not agree to these terms and conditions in full, you must
+            cease using this site immediately.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditionDocs;
