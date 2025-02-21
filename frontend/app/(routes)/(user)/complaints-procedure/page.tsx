f00a9c197b3d9dc9831bb43df86a097ae67ae651
Zubair <zbrlohar@gmail.com>
2025-02-21 20:00:59 +0500

    10

---
+++ frontend/app/(routes)/(user)/complaints-procedure/page.tsx
@@ -0,0 +1,11 @@
+import ComplaintsProcedureDocs from "@/components/newUi/complaintsProcedure/complaintsProcedureDoc";
+
+const ComplaintsProcedure = () => {
+  return (
+    <div>
+      <ComplaintsProcedureDocs />
+    </div>
+  );
+};
+
+export default ComplaintsProcedure;
---
+++ frontend/app/(routes)/(user)/contact-us/page.tsx
@@ -0,0 +1,13 @@
+import ContactUsForm from "@/components/newUi/contactUs/form";
+import ContactUsSocial from "@/components/newUi/contactUs/social";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <ContactUsSocial />
+      <ContactUsForm />
+    </div>
+  );
+};
+
+export default ContactUs;
