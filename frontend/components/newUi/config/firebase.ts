8249ff9197105aaf10b937ecf028177e34c639a1
Zubair <zbrlohar@gmail.com>
2025-02-24 16:14:16 +0500

    Update firebase.ts

--- frontend/components/newUi/config/firebase.ts
+++ frontend/components/newUi/config/firebase.ts
@@ -1,6 +1,43 @@
+// import { getStorage } from "@firebase/storage";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {
+//   FacebookAuthProvider,
+//   GoogleAuthProvider,
+//   getAuth,
+// } from "firebase/auth";
+// import { getFirestore, serverTimestamp } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: "fgrf-dc455",
+//   storageBucket: "fgrf-dc455.appspot.com",
+//   messagingSenderId: "888051572078",
+//   appId: "1:888051572078:web:8bdc54d2fe7ab74953b57f",
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const storage = getStorage(app);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
+// const facebookAuth = new FacebookAuthProvider();
+
+// export {
+//   db,
+//   storage,
+//   serverTimestamp,
+//   auth,
+//   provider,
+//   facebookAuth,
+//   analytics,
+// };
 import { getStorage } from "@firebase/storage";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import {
   FacebookAuthProvider,
   GoogleAuthProvider,
@@ -18,13 +55,25 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const facebookAuth = new FacebookAuthProvider();
+
+// Initialize Analytics only on the client side
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    } else {
+      console.warn("Firebase Analytics is not supported in this environment.");
+    }
+  });
+}
 
 export {
   db,
